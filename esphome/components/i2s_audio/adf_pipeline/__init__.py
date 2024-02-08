1706db54a29d470a293bfade905b48f3ea26d619
Mischa Siekmann <mischa@gnumpi.com>
2024-02-08 22:21:59 +0100

    makes ADFElementI2SIn be configurable

--- esphome/components/i2s_audio/adf_pipeline/__init__.py
+++ esphome/components/i2s_audio/adf_pipeline/__init__.py
@@ -3,7 +3,7 @@ from esphome import pins
 import esphome.config_validation as cv
 
 from ... import adf_pipeline as esp_adf
-from esphome.const import CONF_ID
+from esphome.const import CONF_ID, CONF_CHANNEL
 
 from .. import (
     CONF_I2S_AUDIO_ID,
@@ -15,8 +15,13 @@ from .. import (
     i2s_audio_ns,
 )
 
+CODEOWNERS = ["@gnumpi"]
 AUTO_LOAD = ["adf_pipeline"]
 DEPENDENCIES = ["adf_pipeline", "i2s_audio"]
+
+CONF_SAMPLE_RATE = "sample_rate"
+CONF_BITS_PER_SAMPLE = "bits_per_sample"
+
 
 ADFElementI2SOut = i2s_audio_ns.class_(
     "ADFElementI2SOut",
@@ -34,6 +39,29 @@ ADFElementI2SIn = i2s_audio_ns.class_(
     cg.Component,
 )
 
+i2s_channel_fmt_t = cg.global_ns.enum("i2s_channel_fmt_t")
+CHANNELS = {
+    # Only load data in left channel (mono mode)
+    "left": i2s_channel_fmt_t.I2S_CHANNEL_FMT_ONLY_LEFT,
+    # Only load data in right channel (mono mode)
+    "right": i2s_channel_fmt_t.I2S_CHANNEL_FMT_ONLY_RIGHT,  #
+    # Separated left and right channel
+    "right_left": i2s_channel_fmt_t.I2S_CHANNEL_FMT_RIGHT_LEFT,
+    # Load right channel data in both two channels
+    "all_right": i2s_channel_fmt_t.I2S_CHANNEL_FMT_ALL_RIGHT,
+    # Load left channel data in both two channels
+    "all_left": i2s_channel_fmt_t.I2S_CHANNEL_FMT_ALL_LEFT,
+}
+
+i2s_bits_per_sample_t = cg.global_ns.enum("i2s_bits_per_sample_t")
+BITS_PER_SAMPLE = {
+    16: i2s_bits_per_sample_t.I2S_BITS_PER_SAMPLE_16BIT,
+    32: i2s_bits_per_sample_t.I2S_BITS_PER_SAMPLE_32BIT,
+}
+
+
+_validate_bits = cv.float_with_unit("bits", "bit")
+
 
 CONFIG_SCHEMA_OUT = esp_adf.ADF_COMPONENT_SCHEMA.extend(
     {
@@ -48,6 +76,11 @@ CONFIG_SCHEMA_IN = esp_adf.ADF_COMPONENT_SCHEMA.extend(
         cv.GenerateID(): cv.declare_id(ADFElementI2SIn),
         cv.GenerateID(CONF_I2S_AUDIO_ID): cv.use_id(I2SAudioComponent),
         cv.Required(CONF_I2S_DIN_PIN): pins.internal_gpio_output_pin_number,
+        cv.Optional(CONF_CHANNEL, default="right"): cv.enum(CHANNELS),
+        cv.Optional(CONF_SAMPLE_RATE, default=16000): cv.int_range(min=1),
+        cv.Optional(CONF_BITS_PER_SAMPLE, default="16bit"): cv.All(
+            _validate_bits, cv.enum(BITS_PER_SAMPLE)
+        ),
     }
 )
 
@@ -65,4 +98,8 @@ async def to_code(config):
         cg.add(var.set_dout_pin(config[CONF_I2S_DOUT_PIN]))
     elif config["type"] == "source":
         cg.add(var.set_din_pin(config[CONF_I2S_DIN_PIN]))
+        cg.add(var.set_channel(config[CONF_CHANNEL]))
+        cg.add(var.set_sample_rate(config[CONF_SAMPLE_RATE]))
+        cg.add(var.set_bits_per_sample(config[CONF_BITS_PER_SAMPLE]))
+
     await esp_adf.register_adf_component(var, config)
--- esphome/components/i2s_audio/adf_pipeline/adf_i2s_in.cpp
+++ esphome/components/i2s_audio/adf_pipeline/adf_i2s_in.cpp
@@ -13,9 +13,9 @@ void ADFElementI2SIn::init_adf_elements_() {
 
   i2s_driver_config_t i2s_config = {
       .mode = (i2s_mode_t) (I2S_MODE_MASTER | I2S_MODE_RX),
-      .sample_rate = 16000,
-      .bits_per_sample = I2S_BITS_PER_SAMPLE_16BIT,
-      .channel_format = I2S_CHANNEL_FMT_ONLY_LEFT,  // I2S_CHANNEL_FMT_RIGHT_LEFT,
+      .sample_rate = this->sample_rate_,
+      .bits_per_sample = this->bits_per_sample_,
+      .channel_format = this->channel_,
       .communication_format = I2S_COMM_FORMAT_STAND_I2S,
       .intr_alloc_flags = ESP_INTR_FLAG_LEVEL2 | ESP_INTR_FLAG_IRAM,
       .dma_buf_count = 8,
